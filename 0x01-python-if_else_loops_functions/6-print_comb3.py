3a48668735204ff174292eed57e1034560e279b3
aimane yousr <aimaneyousr@gmail.com>
2023-11-29 14:32:32 +0100

    a program that prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for dig1 in range(10):
+    for dig2 in range(dig1 + 1, 10):
+        if dig1 < 9:
+            print("{:d}{:d} ,".format(dig1, dig2), end="")
+        else:
+            print("{:d}{:d}".format(dig1, dig2))
