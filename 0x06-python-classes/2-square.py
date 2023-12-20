cb927a17e1cd81383c480fa937ef8b8db035c7dd
aimane yousr <aimaneyousr@gmail.com>
2023-12-20 15:27:54 +0100

    a class Square that defines a square by: 'size', where it must be an int and >= 0

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """This is a class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Constructor.
+        Args:
+            size (int): The size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
