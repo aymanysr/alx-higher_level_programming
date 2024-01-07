f1d8f85a29f0163fe04349c9a3a9fa58d8dab4d1
aimane yousr <aimaneyousr@gmail.com>
2024-01-07 13:54:54 +0100

    function that adds two ints, tests: must be ints or floats, else raise TypeError, if float? cast to int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module fot add_integer method"""
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Args:
+        a (int): first integer
+        b (int): second integer
+        Raises:
+            TypeError: if a or b is not an integer or float
+            Returns:
+                The sum of a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
