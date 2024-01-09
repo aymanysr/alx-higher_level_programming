bfa652cff078716a7720b9209879272a6b98f2fa
aimane yousr <aimaneyousr@gmail.com>
2024-01-09 18:30:17 +0100

    print() should print, and str() should return, the square description: [Square] <width>/<height> & main

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module contains a class Square that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class inherits from Rectangle
+    Instantiation with size: def __init__(self, size)
+    size must be private. No getter or setter
+    size must be a positive integer, validated by integer_validator
+    """
+    def __init__(self, size):
+        """Instantiation with size (constructor))"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns string representation of a square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
