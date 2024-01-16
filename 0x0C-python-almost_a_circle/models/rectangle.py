1aa6c79a85ae80e705a6f0828b3250723d3352f3
aimane yousr <aimaneyousr@gmail.com>
2024-01-16 12:11:30 +0100

    fix name of file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,13 +78,17 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
+
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(4, 6)
-    r1.display()
+    r1 = Rectangle(4, 6, 2, 1, 12)
+    print(r1)
 
-    print("---")
-
-    r1 = Rectangle(2, 2)
-    r1.display()
+    r2 = Rectangle(5, 5, 1)
+    print(r2)
