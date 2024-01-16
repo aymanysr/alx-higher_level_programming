1863cfdbb11e4350193e8861b6b549ed1f51146e
aimane yousr <aimaneyousr@gmail.com>
2024-01-16 15:55:36 +0100

    class Square that inherits from Rectangle, __str__ method should return [Square] (<id>) <x>/<y> - <size> - in our case, width or height

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""module for the class 'square'"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """"square representation"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor for the class 'square'"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns a string representation of the class 'square'"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
+
+    print("---")
+
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
+
+    print("---")
+
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
