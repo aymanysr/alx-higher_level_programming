10130579dd063456343deb2e4ff422dbab392f24
aimane yousr <aimaneyousr@gmail.com>
2024-02-15 01:42:59 +0100

    script to create user 'user_0d_1' and grant privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create user_0d_1 if it does not exist & Grant all privileges to user_0d_1
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
