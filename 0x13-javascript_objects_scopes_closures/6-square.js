a5557c303c43f958cb8b398b4e6318f0220783e3
aimane yousr <aimaneyousr@gmail.com>
2024-03-12 16:19:46 +0000

    Add charPrint method to Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the parent class constructor
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
