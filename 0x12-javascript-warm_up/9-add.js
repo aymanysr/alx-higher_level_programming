4e5fad92200ec7c28eb1b1c1f53d770a44742f55
aimane yousr <aimaneyousr@gmail.com>
2024-03-12 00:50:16 +0000

    Add function to perform addition of two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(add(firstArg, secondArg));
