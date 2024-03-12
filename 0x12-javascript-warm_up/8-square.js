49050f7094fe0043862110935ef9add937d59dc7
aimane yousr <aimaneyousr@gmail.com>
2024-03-12 00:30:32 +0000

    refactor a script to print a square of X's based on user input

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
   }
 }
