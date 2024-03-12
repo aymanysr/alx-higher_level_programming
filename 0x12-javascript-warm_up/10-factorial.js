1a5b11b203a02b600d283621ec4c4b32687c664f
aimane yousr <aimaneyousr@gmail.com>
2024-03-12 00:59:20 +0000

    Add factorial function to calculate factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!usr/bin/node
+
+function factorial (n) {
+  // Base case: factorial of 0 or NaN is 1
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  // Recursive case: compute factorial recursively
+  return n * factorial(n - 1);
+}
+
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+console.log(factorial(num));
