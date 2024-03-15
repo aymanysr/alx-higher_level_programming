5c781380c5828232ffef3bbdff057d8ae5c1dbbf
aimane yousr <aimaneyousr@gmail.com>
2024-03-15 20:07:22 +0000

    a script that takes an argument and displays all values in the table where name == that argument + safety from MySQL INJECTIONS

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+        # Create a cursor object
+        cur = db.cursor()
+
+        # SQL query to retrieve states
+        sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+        # Execute SQL query with sys.argv[4] as the state_name parameter
+        cur.execute(sql_query, (sys.argv[4],))
+
+        # Fetch all the rows in a list of tuples
+        rows = cur.fetchall()
+
+        # Display results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close cursor and database connection
+        if 'cur' in locals() or 'cur' in globals():
+            cur.close()
+        if 'db' in locals() or 'db' in globals():
+            db.close()
