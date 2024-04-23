5b765c4fb195d431dba17dcf6f46abb887b91352
aimane yousr <aimaneyousr@gmail.com>
2024-04-24 00:32:29 +0100

    a script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const filesys = require('fs');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    filesys.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
